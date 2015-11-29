118609ee2618c22532b0a334251a9ba67f52e2f1
Miinghan Chen <minghan2@andrew.cmu.edu>
2015-11-28 21:29:51 -0800

    add show relationship and show coauthor functions

---
+++ src/main/java/startingPoint/KG_DBLP/KnowledgeGraph.java
@@ -0,0 +1,110 @@
+package startingPoint.KG_DBLP;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+import neo4j.domain.*;
+import neo4j.repositories.*;
+import neo4j.services.DatasetService;
+import neo4j.services.PaperService;
+
+@Configuration
+@Import(App.class)
+@RestController("/")
+public class KnowledgeGraph extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(KnowledgeGraph.class, args);
+    }
+    
+    @Autowired
+    PaperService paperService;
+    @Autowired
+    DatasetService datasetService;
+
+    @Autowired PaperRepository paperRepository;
+    @Autowired DatasetRepository datasetRepository;
+
+    @RequestMapping("/graph")
+    public Map<String, Object> graph(@RequestParam(value = "limit",required = false) Integer limit) {
+    	return paperService.graph(limit == null ? 100 : limit);
+    }
+    
+    @RequestMapping("/graphTest")
+    public String graphTest(@RequestParam(value = "limit",required = false) String input) {
+    	System.out.println(input);
+    	Map<String, Object> map = null;
+    	if (input == null || input.length() == 0) {
+    		map = paperService.graphAlc(200);
+    	} else {
+    		Integer limit = Integer.parseInt(input);
+    		map = paperService.graphAlc(limit);
+    	}
+    	
+    	String json = "";
+    	ObjectMapper mapper = new ObjectMapper();
+    	try {
+    		//convert map to JSON string
+    		json = mapper.writeValueAsString(map);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return json;
+    }
+    
+    @RequestMapping("/coAuthor")
+    public String getcoAuthor(@RequestParam(value = "name", required = false) String name) {
+    	System.out.println("paper sevice name : " + name);
+    	name = name.replace('+', ' ');
+    	System.out.println("Name: " + name);
+    	Map<String, Object> map = paperService.getCoAuthor(name == null ? "unknown" : name);
+    	System.out.println("/CoAuthor : " + name);
+    	String json = "";
+    	ObjectMapper mapper = new ObjectMapper();
+    	try {
+    		//convert map to JSON string
+    		json = mapper.writeValueAsString(map);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return json;
+    }
+    
+    @RequestMapping("/graphUserDataset")
+    public String graphUserDataset(@RequestParam(value = "limit",required = false) Integer limit) {
+    	Map<String, Object> map = datasetService.graphAlc(limit == null ? 1 : limit);
+    	String json = "";
+    	ObjectMapper mapper = new ObjectMapper();
+    	try {
+    		//convert map to JSON string
+    		json = mapper.writeValueAsString(map);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return json;
+    }
+    
+    @RequestMapping("/getPapers")
+    public Collection<Paper> getPapers(String title) {
+    	return paperRepository.findByTitleContaining(title);
+    	//return paperRepository.findByTitleLike(title);
+    }
+    
+    @RequestMapping("/getPaper")
+    public Paper getPaper(String title) {
+    	//return movieRepository.findByTitleContaining(title);
+    	return paperRepository.findByTitle(title);
+    }
+
+}
